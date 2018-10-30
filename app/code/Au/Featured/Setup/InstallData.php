e9d0b30b00621a6e9080612f1d2e322b3357ee18
quanth6713 <quanth6713@co-well.com.vn>
2018-10-30 13:57:43 +0700

    add module is featured product

---
+++ app/code/Au/Featured/Setup/InstallData.php
@@ -0,0 +1,64 @@
+<?php
+namespace Au\Featured\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+    /**
+     * Init
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+        /* assign object to class global variable for use in other class methods */
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->removeAttribute(\Magento\Catalog\Model\Product::ENTITY,'is_featured');
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'is_featured',
+            [
+                'group' => 'Product Details',
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Is Featured',
+                'input' => 'select',
+                'class' => '',
+                'source' => 'Au\Featured\Model\Config\Source\Options',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => true,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'unique' => false
+            ]
+        );
+    }
+}
